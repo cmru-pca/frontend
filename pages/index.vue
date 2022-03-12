6ec233ad2f583e7d6a20a9ab27de00efb4e34973
sctnightcore <23263315+sctnightcore@users.noreply.github.com>
2022-03-13 02:26:17 +0700

    add diff value

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <v-card class="mt-5">
-      <v-card-title class="text-center justify-center text-h6"
-        >ตารางคะแนน Sci Freshy CMRU 64</v-card-title
-      >
+      <v-card-title class="text-center justify-center text-h6">
+        ตารางคะแนน Sci Freshy CMRU 64
+      </v-card-title>
       <v-card-subtitle class="text-center justify-center overline"
         >อัพเดพเมื่อ: {{ member.updated_at }}
       </v-card-subtitle>
@@ -23,16 +23,55 @@
             {{ item.type }}{{ item.id }}
           </template>
 
+          <template #[`item.name`]="{ item }">
+            <a :href="item.url">
+              {{ item.name }}
+            </a>
+          </template>
+
           <template #[`item.data.point`]="{ item }">
             {{ item.data.point.toLocaleString() }}
+
+            <span
+              class="overline green--text"
+              v-if="item.data.point > item.data.old_point"
+              >(+{{ item.data.point - item.data.old_point }})</span
+            >
+            <span
+              class="overline red--text"
+              v-if="item.data.old_point > item.data.point"
+              >(-{{ item.data.old_point - item.data.point }})</span
+            >
           </template>
 
           <template #[`item.data.like`]="{ item }">
             {{ item.data.like.toLocaleString() }}
+
+            <span
+              class="overline green--text"
+              v-if="item.data.like > item.data.old_like"
+              >(+{{ item.data.like - item.data.old_like }})</span
+            >
+            <span
+              class="overline red--text"
+              v-if="item.data.old_like > item.data.like"
+              >(-{{ item.data.old_like - item.data.like }})</span
+            >
           </template>
 
           <template #[`item.data.share`]="{ item }">
             {{ item.data.share.toLocaleString() }}
+
+            <span
+              class="overline green--text"
+              v-if="item.data.share > item.data.old_share"
+              >(+{{ item.data.share - item.data.old_share }})</span
+            >
+            <span
+              class="overline red--text"
+              v-if="item.data.old_share > item.data.share"
+              >(-{{ item.data.old_share - item.data.share }})</span
+            >
           </template>
         </v-data-table>
       </v-card-text>
